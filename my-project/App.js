3ec34f69c313fa7e4c8de504fc70d0a12d88c827
NowhereCat <gabrielbportela413@gmail.com>
2023-03-26 16:44:34 -0300

    Update App.js

--- my-project/App.js
+++ my-project/App.js
@@ -5,16 +5,21 @@ import { FlatList, ScrollView, StyleSheet, Text, View, TouchableOpacity } from '
 export default function App() {
 
   const [count, setCount] = useState([
-    {name: '0', key: 0},
-    {name: '1', key: 1},
-    {name: '2', key: 2},
-    {name: '3', key: 3},
-    {name: '4', key: 4},
-    {name: '5', key: 5},
-    {name: '6', key: 6},
-    {name: '7', key: 7},
-    {name: '8', key: 8},
-    {name: '9', key: 9},
+    {name: 'One', key: 1},
+    {name: 'Two', key: 2},
+    {name: 'Three', key: 3},
+    {name: 'Four', key: 4},
+    {name: 'Five', key: 5},
+    {name: 'Six', key: 6},
+    {name: 'Seven', key: 7},
+    {name: 'Eight', key: 8},
+    {name: 'Nine', key: 9},
+    {name: 'Ten', key: 10},
+    {name: 'Eleven', key: 11},
+    {name: 'Twelve', key: 12},
+    {name: 'Thirteen', key: 13},
+    {name: 'Fourteen', key: 14},
+    {name: 'Fifteen', key: 15},
   ])
 
   const apertarBotao = (key) => {
@@ -29,14 +34,19 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.titleText}>Add Gateway</Text>
       <FlatList 
-        numColumns={2}
+        numColumns={3}
         keyExtractor={(item) => item.key}
         data={count}
         renderItem={({item}) => (
           <Text style={styles.item}>{item.name}</Text>
         )}
         style={styles.flatListContainer}
-        columnWrapperStyle={{alignItems: 'stretch'}}
+        columnWrapperStyle={{
+          flex: 1,
+          alignItems: 'center',
+          alignSelf: 'center',
+          backgroundColor: 'silver',
+        }}
       />
     </View>
     
@@ -46,6 +56,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
+    alignItems: 'center',
   },
   titleText:{
     textAlign: 'center',
@@ -54,16 +65,18 @@ const styles = StyleSheet.create({
   },
   flatListContainer:{
     backgroundColor: 'silver',
+    width: '100%',
+    height: '100%',
   },
   item: {
-    marginTop: 24,
-    marginRight: 30,
-    padding: 40,
-    paddingLeft: 85,
-    paddingRight: 85,
-    backgroundColor: 'magenta',
-    fontSize: 24,
+    margin: 5,
+    width: 125,
+    height: 150,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    backgroundColor: 'blue',
+    fontSize: 18,
     fontWeight: 'bold',
-    alignItems: 'stretch',
+    color: 'white',
   },
 });
